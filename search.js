6d537b1c78d45594a440c84b15ceef010bb327a0
Abhishek <shanti7482@gmail.com>
2023-03-16 11:51:50 +0530

    initial commit

---
+++ search.js
@@ -0,0 +1,7 @@
+const searchBarContainerEl = document.querySelector(".search-bar-container");
+
+const magnifierEl =document.querySelector(".magnifier");
+
+/*magnifierEl.addEventListener("click",->{
+    searchBarContainerElclassList.toggle("active");
+});*/
